c94af2a7f31dfca043b226049f219a2f66de49af
d0x471b <82776645+0x471@users.noreply.github.com>
2022-04-05 23:39:53 +0300

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 require('@nomiclabs/hardhat-ethers');
-const { mnemonic } = require('./secrets.json');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -30,13 +29,11 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
     },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
     }
   },
   solidity: {
