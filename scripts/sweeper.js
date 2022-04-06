9b19e0cefd9e20ac9cd1c98e94fdd330b3f1263a
0x471 <0x471@protonmail.com>
2022-04-06 20:30:01 +0300

    update sweeper.js

--- scripts/sweeper.js
+++ scripts/sweeper.js
@@ -1,52 +1,29 @@
-var fs = require('fs')
-var Tx = require('ethereumjs-tx').Transaction;
-var Web3 = require('web3')
-var Common = require('ethereumjs-common').default;
+const ethers = require('ethers')
 
-var web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545'))
-var BSC_FORK = Common.forCustomChain(
-    'mainnet',
-    {
-        name: 'bnb',
-        networkId: 97,
-        chainId: 97,
-        url: 'https://data-seed-prebsc-1-s1.binance.org:8545'
-    },
-    'istanbul',
-);
 
-var originalAmountToSend = '0.01';
-var amountToSend = web3.utils.toWei(originalAmountToSend, 'ether');
-var sourceAccount = JSON.parse(fs.readFileSync('FROM.json', 'utf-8'));
-var targetAccount = JSON.parse(fs.readFileSync('TO.json', 'utf-8'));
-console.log(sourceAccount, targetAccount)
+async function main() {
+    let network = 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    let provider = ethers.getDefaultProvider(network)
 
-async function sendBNB(fromAddress, toAddress, pKey, amountToSend) {
-    console.log("send bnb function executed")
-    var privateKey = Buffer.from(pKey.slice(2), 'hex');
-    var count = await web3.eth.getTransactionCount(fromAddress);
+    let privKey = "SENDER-PRIVATE_KEY"
+    let wallet = new ethers.Wallet(privKey, provider)
 
-    var rawTransaction = {
-        "from": fromAddress,
-        "gasPrice": web3.utils.toHex(63000000000),//?
-        "gasLimit": web3.utils.toHex(300000), //?
-        "to": toAddress,
-        "value": web3.utils.toHex(amountToSend),
-        "nonce": web3.utils.toHex(count)
-    };
+    let receiver = "RECEIVER-WALLET-ADDR"
+    let amountInEther = '1.0'
 
-    var transaction = new Tx(rawTransaction, { 'common': BSC_FORK });
-    transaction.sign(privateKey);
+    let tx = {
+        to: receiver,
+        value: ethers.utils.parseEther(amountInEther)
+    }
 
-    var result = await web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'));
-    console.log(result);
-    return result;
+    wallet.sendTransaction(tx)
+    .then((txObj) => {
+        console.log('txHash:', txObj.hash)
+    })
 }
 
-sendBNB(sourceAccount.address, targetAccount.address, sourceAccount.privateKey, amountToSend)
-    .then(() => process.exit(0))
-    .catch((error) => {        
-        console.error(error);
-        process.exit(1);
+main()
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
     })
-
